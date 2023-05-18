7f1ea0cef65d4dbdbbff49d9c8844e31353f2ea4
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-05-18 15:34:07 +0200

    fix: avoid moving files to root user's Trash
    Resolves: https://github.com/tshakalekholoane/Can/issues/1

--- Sources/Can/Can.swift
+++ Sources/Can/Can.swift
@@ -2,6 +2,39 @@ import Foundation
 
 @main
 public enum Can {
+  /// Represents a system error encountered during an operation and
+  /// provides a Swift representation of a C error.
+  struct SystemError: Error {
+    let errno: Int32
+    var localizedDescription: String {
+      String(cString: strerror(errno))
+    }
+  }
+
+  /// Reverts the effective user ID (UID) of the process to the original
+  /// value after running with elevated privileges. This is to avoid
+  /// using the root user's Trash.
+  ///
+  /// - Returns: A `Result` object representing the success or failure
+  ///   of the operation.
+  ///   - On success, it returns `Void`.
+  ///   - On failure, it returns a `SystemError` indicating the
+  ///     encountered error.
+  private static func revertToOriginalUserID() -> Result<Void, SystemError> {
+    let processInfo = ProcessInfo()
+    guard let sudoUser = processInfo.environment["SUDO_USER"] else {
+      // Not running with elevated privileges.
+      return .success(())
+    }
+
+    guard let uid = getpwnam(sudoUser)?.pointee.pw_uid else {
+      // Failed to retrieve the user ID associated with the SUDO_USER.
+      return .failure(SystemError(errno: errno))
+    }
+
+    return seteuid(uid) != 0 ? .failure(SystemError(errno: errno)) : .success(())
+  }
+
   private static func trash(_ filenames: [String]) {
     filenames.forEach { filename in
       let fileManager = FileManager.default
@@ -15,6 +48,13 @@ public enum Can {
   }
 
   public static func main() {
+    switch revertToOriginalUserID() {
+    case .success: break
+    case let .failure(error):
+      eprint(error.localizedDescription)
+      exit(1)
+    }
+
     switch Options.parseArguments() {
     case let .success(files):
       trash(files)
